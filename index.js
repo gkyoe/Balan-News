f0d942271588594b67a00f891f29753d499fa576
곽효은 <gwakhyoeun@gmail.com>
2020-05-05 16:46:38 +0900

    [index.js에서 서버실행코드 분리 -> server.js] - server.js 파일 생성 - 환경변수 test 시, 서버 로그 안 찍히도록 index.js에 코드 추가

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 
-app.use(morgan("dev"));
+if (process.env.NODE_ENV !== "test") {
+  app.use(morgan("dev"));
+}
 
 app.get("/", (req, res) => {
   res.status(200);
   res.send("Hello World!");
-});
-
-app.listen(3000, () => {
-  console.log("server is running");
 });
 
 module.exports = app;
