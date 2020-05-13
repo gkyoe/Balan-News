6afcabbfa17e77e26531a1eb60af45ed9233ed74
곽효은 <gwakhyoeun@gmail.com>
2020-05-13 23:31:56 +0900

    [클라이언트 초기설정 셋팅] - react, react-dom 및 @types 설치 - webpack,webpack-cli, webpack-dev-server, ts-loader 설치 - tsconfig.json 설정 변경 - webpack.config.js 설정 입력 - index.html, app.tsx 생성

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  mode: "development",
+  entry: "./src/client/app.tsx",
+  output: {
+    path: __dirname + "/dist/client",
+    filename: "app.js",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  devServer: {
+    contentBase: "./",
+    publicPath: "/dist/client",
+    inline: true,
+    hot: true,
+  },
+  module: {
+    rules: [{ test: /\.tsx?$/, loader: "ts-loader" }],
+  },
+};
