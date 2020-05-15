a8b6e34a3516b6c847e23a8d847f5cc7d73762bd
곽효은 <gwakhyoeun@gmail.com>
2020-05-15 21:53:59 +0900

    [회원가입시 유저 확인 로직 작성] - user-controller.ts 코드 작성 - package.json 에서 'npm run server'시 경로 재설정

--- src/server/controllers/user-controller.ts
+++ src/server/controllers/user-controller.ts
@@ -1,18 +1,35 @@
 import { Request, Response } from "express";
 import mongoose from "mongoose";
 import * as User from "../models/user";
+import { runInNewContext } from "vm";
 
 export default class UserController {
-  public signup(req: Request, res: Response) {
+  public async signup(req: Request, res: Response) {
     const { email, password } = req.body;
-
-    User.UserModel.find()
-      .exists(email, true)
-      .then((result) => {
-        console.log(`${result}님은 이미 가입한 유저입니다.`);
-      })
-      .catch((err: Error) => {
+    console.log(email, password);
+    // res.status(200).send("ok done!");
+    try {
+      await User.UserModel.find()
+        .exists(email, true)
+        .then((result) => {
+          if (result) res.status(409).send("이미 가입한 유저입니다.");
+          else {
+            User.UserModel.create({
+              email,
+              password,
+            });
+          }
+        })
+        .then((check: void) => {
+          res.json(check).status(200);
+        })
+        .catch((err: Error) => {
+          throw err;
+        });
+    } catch {
+      (err: Error) => {
         throw err;
-      });
+      };
+    }
   }
 }
