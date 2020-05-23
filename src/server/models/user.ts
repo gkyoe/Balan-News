cd22e9d9dfce7d02e904d8bd74d440ad0ab49103
곽효은 <gwakhyoeun@gmail.com>
2020-05-23 09:03:38 +0900

    [user.ts 에 비밀번호 해쉬 저장 로직 추가] - bcrypt 모듈 설치 - pre() 함수로 비밀번호 저장될 경우 해쉬에서 저장하는 로직 추가

--- src/server/models/user.ts
+++ src/server/models/user.ts
@@ -1,5 +1,8 @@
 import { Document, Model, model, Types, Schema, Query } from "mongoose";
+import bcrypt from "bcrypt";
 import { ArticleModel } from "./article";
+
+const SALT_WORK_FACTOR = 10;
 
 export interface IUserSchema extends Document {
   mail: string;
@@ -35,5 +38,34 @@ const UserSchema = new Schema(
   }
 );
 
+UserSchema.pre("save", function (this: IUserSchema, next) {
+  const _user = this;
+  if (!_user.isModified("password")) return next();
+  // generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+    if (err) return next(err);
+
+    // hash the password using our new salt
+    bcrypt.hash(_user.password, salt, function (err, hash) {
+      if (err) return next(err);
+      // _user.password 에 red line 에러 => this 에 IUserSchema 주고 해결
+      // https://stackoverflow.com/questions/46182826/mongoose-hooks-not-working-with-typescript
+
+      // override the cleartext password with the hashed one
+      _user.password = hash;
+      next();
+    });
+  });
+});
+
+UserSchema.methods.comparePassword = function (
+  candidatePassword: string,
+  cb: any
+) {
+  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
 export const user = model<IUserSchema>("user", UserSchema);
-// model의 첫번째 인자는 컬렉션의 이름,
