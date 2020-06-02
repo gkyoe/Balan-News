12d048ee54c3c9245f5db9f88fd6f4bad0fda06e
곽효은 <gwakhyoeun@gmail.com>
2020-06-02 13:15:00 +0900

    [네이버 뉴스 API 연결 & select 함수 작성] - 네이버 오픈 api 생성 및 env에 id, secretKey 입력 - 뉴스 GET 요청 시 header 설정 - query 전송시 utf-8 로 인코딩하기 위해 "urlencode" 모듈 설치 - axios GET 요청으로 "날씨" 라는 검색어로 data 불러오기 성공

--- dist/server/controllers/article-controller.js
+++ dist/server/controllers/article-controller.js
@@ -1,24 +1,105 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Controller = /** @class */ (function () {
-    function Controller() {
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
-    Controller.prototype.index = function (req, res) {
-        res.status(200);
-        res.send("Hello world");
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var axios_1 = __importDefault(require("axios"));
+var urlencode_1 = __importDefault(require("urlencode"));
+var dotenv = __importStar(require("dotenv"));
+dotenv.config();
+var secret = process.env.secret;
+var articleController = /** @class */ (function () {
+    function articleController() {
+    }
+    articleController.prototype.select = function (req, res) {
+        return __awaiter(this, void 0, void 0, function () {
+            var subject, encoded, api_url, client_id, client_scret, options;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        subject = "날씨";
+                        encoded = urlencode_1.default(subject);
+                        console.log(encoded); //%EB%82%A0%EC%94%A8
+                        api_url = "https://openapi.naver.com/v1/search/news.json?query=" + encoded;
+                        client_id = process.env.naverNewsApi_id;
+                        client_scret = process.env.naverNewsApi_ScretKey;
+                        options = {
+                            // url: api_url,
+                            headers: {
+                                "X-Naver-Client-Id": client_id,
+                                "X-Naver-Client-Secret": client_scret,
+                            },
+                        };
+                        // https://openapi.naver.com/v1/search/news.json?query=%EB%82%A0%EC%94%A8
+                        return [4 /*yield*/, axios_1.default
+                                .get("https://openapi.naver.com/v1/search/news.json?query=" + encoded, options)
+                                .then(function (res) { return console.log("res.data: ", res.data); })
+                                .catch(function (err) {
+                                console.log("err: ", err);
+                            })];
+                    case 1:
+                        // https://openapi.naver.com/v1/search/news.json?query=%EB%82%A0%EC%94%A8
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
     };
-    Controller.prototype.login = function (req, res) {
-        res.status(200);
-        res.send("plz login!");
-    };
-    Controller.prototype.signup = function (req, res) {
-        res.status(200);
-        res.send("plz signup!");
-    };
-    Controller.prototype.logout = function (req, res) {
-        res.status(200);
-        res.send("See you again!");
-    };
-    return Controller;
+    return articleController;
 }());
-exports.default = Controller;
+exports.default = articleController;
